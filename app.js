b5da0e03cbf1fd82f1eadae50434c112a35769fe
thuongb14 <thuongb14@gmail.com>
2022-06-13 22:56:34 +1000

    Add .

--- app.js
+++ app.js
@@ -1,17 +1,86 @@
-const display = document.querySelector('.display');
 const number = document.querySelectorAll('.number');
+const keys = document.querySelector('.keypad')
+const display = document.querySelector('.display');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equal');
+const dot = document.querySelector('.decimal')
 
-function display() {
-    
-}
+display.textContent = '0';
 
-let current = [];
+let previousNumber = '';
+let currentNumber = '';
+let operator = '';
 
+// if num clicked, display Num
 number.forEach((num) => {
     num.addEventListener('click', function(e) {
-        display.textContent += e.target.value;
-        current.push(e.target.value);
+        displayNum(e.target.textContent)
+    })
+    
+})
+
+function displayNum(num){
+    if(previousNumber !== '' && currentNumber !== '' && operator === '') {
+        previousNumber = '';
+        display.textContent = currentNumber;
+    }
+    if(currentNumber.length <=13) {
+        currentNumber += num;
+        display.textContent = currentNumber;
+    } else {
+        display.textContent = 'Overloaded!';
+    }
+}
+
+//if operator clicked, store operator
+operators.forEach((ops) => {
+    ops.addEventListener('click', (e) => {   
+        operatorInput(e.target.textContent)
     })
 })
 
-let currentNumber = +current.join('')
+function operatorInput(op) {
+    previousNumber = currentNumber;
+    currentNumber = ''
+    operator = op;
+}
+
+// if equal click, calculate from num and operator
+equals.addEventListener('click', () => {
+    if (currentNumber != '' && previousNumber != '') {
+        operate()
+    }
+});
+
+
+function operate() {
+    if (operator === '+') {
+        previousNumber = Number(previousNumber) + Number(currentNumber);
+    } else if (operator === '-'){
+        previousNumber = Number(previousNumber) - Number(currentNumber);
+    } else if (operator === 'x') {
+        previousNumber = Number(previousNumber) * Number(currentNumber);
+    } else if (operator === '/') {
+        previousNumber = Number(previousNumber) / Number(currentNumber);
+
+    };
+    display.textContent = roundNumber(previousNumber)
+    currentNumber = previousNumber.toString();
+    displayResult()
+}
+
+//round big num
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000
+}
+
+//round float
+function displayResult() {
+    if(currentNumber.length <= 13) {
+        display.textContent = currentNumber;
+    } else{
+        display.textContent = currentNumber.slice(0, 13) + '...';
+    }
+}
+
+
